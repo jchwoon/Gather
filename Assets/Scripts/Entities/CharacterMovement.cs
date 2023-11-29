ff5eccf09c6ca636abc390bcc1ac8fdf8a69d5d3
정채운 <toyboy01113@gmail.com>
2023-11-29 10:08:07 +0900

    fix: 캐릭터 이동 기능 수정

--- Assets/Scripts/Entities/CharacterMovement.cs
+++ Assets/Scripts/Entities/CharacterMovement.cs
@@ -1,52 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    private MoveCharacterController _controller;
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _characterSprite;
     private Vector2 _movementDirection = Vector2.zero;
+    private MoveInput _moveInput = null;
+    private float moveSpeed = 5f;
+
+    //X축 방향 데이타
+    private string currentXKey = null;
+    private int leftCount = 0;
+    private int rightCount = 0;
+    private bool isPressingBothX = false;
+
+    //Y축 방향 데이타
+    private string currentYKey = null;
+    private int upCount = 0;
+    private int downCount = 0;
+    private bool isPressingBothY = false;
 
     private void Awake()
     {
-        _controller = GetComponent<MoveCharacterController>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _characterSprite = GetComponent<SpriteRenderer>();
         _characterSprite = transform.Find("CharacterSprite").GetComponent<SpriteRenderer>();
+        _moveInput = new MoveInput();
     }
-    void Start()
+
+    private void OnEnable()
     {
-        _controller.OnMoveEvent += Move;
+        _moveInput.Enable();
+        _moveInput.Player.Move.performed += OnMoveMentPerformed;
+        _moveInput.Player.Move.canceled += OnMoveMentCancelled;
     }
 
     private void FixedUpdate()
     {
-        ApplyMove(_movementDirection);
+        _rigidbody.velocity = _movementDirection * moveSpeed;
     }
 
-    private void Move(Vector2 direction)
+    private void OnDisable()
     {
-        FlipCharacterDirection(direction);
-        _movementDirection = direction;
+        _moveInput.Disable();
+        _moveInput.Player.Move.performed -= OnMoveMentPerformed;
     }
 
-    private void FlipCharacterDirection(Vector2 direction)
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    private void OnMoveMentPerformed(InputAction.CallbackContext callbackContext)
     {
-        if (direction.x == 1)
+        string controlName = callbackContext.control.name;
+
+        if (controlName == "a" || controlName == "d")
+        {
+            CheckDirectionPriority(controlName, "a", "d", true, ref leftCount, ref rightCount,ref currentXKey ,ref isPressingBothX, ref _movementDirection.x);
+        }
+        
+        if (controlName == "w" || controlName == "s")
+        {
+            CheckDirectionPriority(controlName, "w", "s", false, ref upCount, ref downCount,ref currentYKey, ref isPressingBothY, ref _movementDirection.y);
+        }
+
+        FlipCharacterDirection();
+    }
+
+    private void OnMoveMentCancelled(InputAction.CallbackContext callbackContext)
+    {
+        _movementDirection = Vector2.zero;
+    }
+
+    private void FlipCharacterDirection()
+    {
+        if (_movementDirection.x > 0)
         {
             _characterSprite.flipX = false;
         }
-
-        if (direction.x == -1)
+        else if (_movementDirection.x < 0)
         {
             _characterSprite.flipX = true;
         }
     }
 
-    private void ApplyMove(Vector2 direction)
+    private void CheckDirectionPriority(string controlName, string compareA, string compareB, bool isXAxis, ref int countA, ref int countB, ref string currentKey, ref bool isPressingBoth, ref float direction)
     {
-        _rigidbody.velocity = direction;
+        //compare A,B 둘 다 누루고 있는 상황에서 A가 입력으로 들어올 때
+        if (controlName == compareA)
+        {
+            if (isPressingBoth) currentKey = compareB;
+
+            countA++;
+            if (countA == 1) currentKey = controlName;
+            if (countA == 2) countA = 0;
+        }
+
+        //compare A,B 둘 다 누루고 있는 상황에서 B가 입력으로 들어올 때
+        if (controlName == compareB)
+        {
+            if (isPressingBoth) currentKey = compareA;
+
+            countB++;
+            if (countB == 1) currentKey = controlName;
+            if (countB == 2) countB = 0;
+        }
+
+        //A, B 둘 다 떼어져 있을 때는 0 아니면 방향 설정
+        if (countB == 0 && countA == 0) direction = 0;
+        else
+        {
+            if (currentKey == compareA) direction = isXAxis ? -1 : 1;
+            else direction = isXAxis ? 1 : -1;
+        }
+
+        isPressingBoth = countA == 1 && countB == 1;
     }
 }
